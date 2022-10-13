4a212d96f52f2371cc6ed0f5b7acbce5e46eea3b
meli-imelda <meliimeldatchouala@gmail.com>
2022-10-13 14:12:22 +0100

    RollyJs & AOSJs libraries for scroll & animation effects

--- Bellissima/js/bellissima.js
+++ Bellissima/js/bellissima.js
@@ -5,3 +5,15 @@ $('.sec3__slider').slick({
     speed:1500
   });
       
+// Initialising the Rolly js library
+  const r = rolly({
+    view: document.querySelector('.app'),
+    native: true,
+    // other options
+  });
+  r.init();
+
+// Initialising the AOS.js library
+AOS.init({
+    duration: 1500 //global duration
+});
