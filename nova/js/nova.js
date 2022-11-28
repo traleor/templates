9e2cf5dbcbae5397ec4d85c7b028895b4ad0159d
Meli Imelda <meliimeldatchouala@gmail.com>
2022-11-28 19:35:19 +0100

    changed primary colors

--- nova/js/nova.js
+++ nova/js/nova.js
@@ -1,1 +1,15 @@
 console.log("Traleor Nova Js: Create your Free website at https://traleor.com");
+
+
+// Initialising the Rolly js library
+const r = rolly({
+    view: document.querySelector('.app'),
+    native: true,
+    // other options
+  });
+  r.init();
+
+// Initialising the AOS.js library
+AOS.init({
+    duration: 1500 //global duration
+});
